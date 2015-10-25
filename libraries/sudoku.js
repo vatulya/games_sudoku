16b761e21d67ab26b4d6773cb930d02711e7a810
Vitalii Vatulia <vatvit@gmail.com>
2015-10-25 17:34:27 +0200

    refactor libraries/sudoku to ES6

--- libraries/sudoku.js
+++ libraries/sudoku.js
@@ -1,141 +1,143 @@
-var extend = require('util')._extend;
+"use strict";
 
-var ModelSudoku = require('./../models/sudoku'),
+let extend = require('util')._extend;
+
+let ModelSudoku = require('./../models/sudoku'),
     ModelSudokuBoard = require('./../models/sudoku/board'),
     SudokuBoard = require('./sudoku/board'),
     SudokuHistory = require('./sudoku/history');
 
-function Sudoku(modelSudoku, parameters) {
-    this.modelSudoku = modelSudoku;
-    this.board = null;
-    this.history = null;
+let Sudoku = class {
 
-    if (typeof parameters === 'object') {
-        if (parameters.hasOwnProperty('board') && parameters.board instanceof SudokuBoard) {
-            this.board = parameters.board;
-        }
-        if (parameters.hasOwnProperty('history') && parameters.history instanceof SudokuHistory) {
-            this.history = parameters.history;
+    constructor (modelSudoku, parameters) {
+        this.modelSudoku = modelSudoku;
+        this.board = null;
+        this.history = null;
+
+        if (typeof parameters === 'object') {
+            if (parameters.hasOwnProperty('board') && parameters.board instanceof SudokuBoard) {
+                this.board = parameters.board;
+            }
+            if (parameters.hasOwnProperty('history') && parameters.history instanceof SudokuHistory) {
+                this.history = parameters.history;
+            }
         }
     }
-}
 
-/********************************************** PROTECTED METHODS ***/
+    /********************************************** STATIC METHODS ***/
 
-Sudoku.prototype.getSystemData = function () {
-
-    return {
-        _system: {
-            gameHash: this.getHash(),
-            undoMove: this.history.undo,
-            redoMove: this.history.redo,
-            duration: 15, // this.duration,
-            microtime: new Date().getTime(),
-            resolved: this.board.isResolved()
-        }
-    };
-};
-/********************************************** /PROTECTED METHODS ***/
-
-/********************************************** PUBLIC METHODS ***/
-
-Sudoku.prototype.getHash = function () {
-    return this.modelSudoku.hash;
-};
-
-Sudoku.prototype.getSize = function () {
-    return this.board.size;
-};
-
-Sudoku.prototype.getCellByCoords = function (row, col) {
-    return this.board.getCellByCoords(row, col);
-};
-
-Sudoku.prototype.doUserAction = function (data, callback) {
-    var self = this,
-        toCells = {},
-        diff = {};
-
-    if (!this.board.isCorrectParameters(data || {})) {
-        return callback(new Error('Wrong user action data'));
-    }
-
-    toCells = SudokuBoard.createCellsFromBoardHash(extend(data, {size: this.board.size}));
-
-    diff = this.history.getDiff(this.board.cells, toCells);
-
-    if (!Object.keys(diff.checkedCells).length && !Object.keys(diff.markedCells).length) {
-        callback(new Error('Saving data error'));
-    }
-
-    this.board.applyDiff(diff, function (error) {
-        if (error) { return callback(error); }
-        self.history.addAction(diff, function (error) {
+    static load (hash, callback) {
+        ModelSudoku.findOneByHash(hash, function (error, modelSudoku) {
             if (error) { return callback(error); }
-            callback(null);
-        });
-    });
-};
+            if (!modelSudoku) { return callback(new Error('Wrong hash')); }
 
-/********************************************** PUBLIC METHODS ***/
-
-/********************************************** STATIC METHODS ***/
-
-Sudoku.create = function (hash, callback) {
-    var parameters,
-        sudoku,
-        modelSudoku;
-
-    if (typeof hash !== 'string' || !hash) {
-        return callback(new Error('Wrong hash'));
-    }
-
-    modelSudoku = new ModelSudoku();
-    modelSudoku.set('hash', hash);
-
-    sudoku = new Sudoku(modelSudoku);
-
-    // Sync set board and set history
-    parameters = {
-        size: 9
-    };
-    SudokuBoard.create(parameters, function (error, sudokuBoard) {
-        if (error) { return callback(error); }
-
-        modelSudoku.set('boardId', sudokuBoard.getId());
-
-        SudokuHistory.create(function (error, sudokuHistory) {
-            if (error) { return callback(error); }
-
-            modelSudoku.set('historyId', sudokuHistory.getId());
-
-            modelSudoku.save(function (error) {
+            SudokuBoard.load(modelSudoku.get('boardId'), function (error, sudokuBoard) {
                 if (error) { return callback(error); }
 
-                callback(null, sudoku);
+                SudokuHistory.load(modelSudoku.historyId, function (error, sudokuHistory) {
+                    if (error) { return callback(error); }
+                    if (!modelSudoku) { return callback(new Error('Wrong board ID')); }
+
+                    callback(null, new Sudoku(modelSudoku, {board: sudokuBoard, history: sudokuHistory}));
+                });
             });
         });
-    });
-};
+    }
 
-Sudoku.load = function (hash, callback) {
-    ModelSudoku.findOneByHash(hash, function (error, modelSudoku) {
-        if (error) { return callback(error); }
-        if (!modelSudoku) { return callback(new Error('Wrong hash')); }
+    static create (hash, callback) {
+        let parameters,
+            sudoku,
+            modelSudoku;
 
-        SudokuBoard.load(modelSudoku.get('boardId'), function (error, sudokuBoard) {
+        if (typeof hash !== 'string' || !hash) {
+            return callback(new Error('Wrong hash'));
+        }
+
+        modelSudoku = new ModelSudoku();
+        modelSudoku.set('hash', hash);
+
+        sudoku = new Sudoku(modelSudoku);
+
+        // Sync set board and set history
+        parameters = {
+            size: 9
+        };
+        SudokuBoard.create(parameters, function (error, sudokuBoard) {
             if (error) { return callback(error); }
 
-            SudokuHistory.load(modelSudoku.historyId, function (error, sudokuHistory) {
-                if (error) { return callback(error); }
-                if (!modelSudoku) { return callback(new Error('Wrong board ID')); }
+            modelSudoku.set('boardId', sudokuBoard.getId());
 
-                callback(null, new Sudoku(modelSudoku, {board: sudokuBoard, history: sudokuHistory}));
+            SudokuHistory.create(function (error, sudokuHistory) {
+                if (error) { return callback(error); }
+
+                modelSudoku.set('historyId', sudokuHistory.getId());
+
+                modelSudoku.save(function (error) {
+                    if (error) { return callback(error); }
+
+                    callback(null, sudoku);
+                });
             });
         });
-    });
-};
+    }
 
-/********************************************** /STATIC METHODS ***/
+    /********************************************** /STATIC METHODS ***/
+
+    /********************************************** PUBLIC METHODS ***/
+
+    getHash () {
+        return this.modelSudoku.hash;
+    }
+
+    getSize () {
+        return this.board.size;
+    }
+
+    getCellByCoords (row, col) {
+        return this.board.getCellByCoords(row, col);
+    }
+
+    doUserAction (data, callback) {
+        let self = this,
+            toCells = {},
+            diff = {};
+
+        if (!this.board.isCorrectParameters(data || {})) {
+            return callback(new Error('Wrong user action data'));
+        }
+
+        toCells = SudokuBoard.createCellsFromBoardHash(extend(data, {size: this.board.size}));
+
+        diff = this.history.getDiff(this.board.cells, toCells);
+
+        if (!Object.keys(diff.checkedCells).length && !Object.keys(diff.markedCells).length) {
+            callback(new Error('Saving data error'));
+        }
+
+        this.board.applyDiff(diff, function (error) {
+            if (error) { return callback(error); }
+            self.history.addAction(diff, function (error) {
+                if (error) { return callback(error); }
+                callback(null);
+            });
+        });
+    }
+
+    getSystemData () {
+        return {
+            _system: {
+                gameHash: this.getHash(),
+                undoMove: this.history.undo,
+                redoMove: this.history.redo,
+                duration: 15, // this.duration,
+                microtime: new Date().getTime(),
+                resolved: this.board.isResolved()
+            }
+        };
+    }
+
+    /********************************************** /PUBLIC METHODS ***/
+
+};
 
 module.exports = Sudoku;
