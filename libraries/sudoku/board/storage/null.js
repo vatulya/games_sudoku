c48418beb6d689f9a554f23b12617e34fa65d654
Vitalii Vatulia <vatvit@gmail.com>
2015-12-20 22:18:03 +0200

    remove extra code from Board storage null

--- libraries/sudoku/board/storage/null.js
+++ libraries/sudoku/board/storage/null.js
@@ -11,13 +11,6 @@ let nullStorage = class {
     }
 
     save(parameters, callback) {
-        let keys = Object.keys(parameters);
-
-        model.set('size', parameters.size);
-        model.set('openedCells', parameters.openedCells);
-        model.set('checkedCells', parameters.checkedCells);
-        model.set('markedCells', parameters.markedCells);
-        model.set('squares', parameters.squares);
         callback(null);
     }
 
